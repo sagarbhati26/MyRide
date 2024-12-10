50e62591279c098579f789a0f00e97d574981949
sagarbhati26 <sagarbhati2616@gmail.com>
2024-12-11 01:11:01 +0530

    utils

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,14 @@
+
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        const statusCode = error.code || 500;
+        const message = error.message || 'Internal Server Error';
+        res.status(statusCode).json({
+            success: false,
+            message,
+            statusCode
+        });
+    }
+};
